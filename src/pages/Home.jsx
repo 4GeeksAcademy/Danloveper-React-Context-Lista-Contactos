f70e3a7872a129fa4f10d7ee2c564ac70a4d9240
William Vargas <danloveper@gmail.com>
2025-08-08 21:42:06 +0000

    Add/template card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,35 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container">
+			<div className="card w-100">
+				<div className="row g-0">
+					<div className="col-md-4 d-flex justify-content-center">
+						<img src="https://picsum.photos/id/237/200/300" className="my-2 object-fit-cover rounded-circle" alt="..." style={{ width: "200px", height: "200px" }} />
+					</div>
+					<div className="col-md-4">
+						<div className="card-body">
+							<p className="display-6">Mike Anamendolla</p>
+							<div className="card-text text-secondary" style={{fontSize:'1.2rem'}}>
+								<p><i class="fa-solid fa-location-dot pe-2"></i> 5242 Hillcrest Rd</p>
+								<p><i class="fa-solid fa-phone pe-2"></i> (870) 288-4149</p>
+								<p><i class="fa-solid fa-envelope pe-2"></i> mike.anaexample</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div className="card-body display-3">
+							<i class="fa-solid fa-pencil"></i>
+							<i class="fa-solid fa-trash-can"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
 		</div>
 	);
 }; 
